30e403c2f82d526d8ef2d435f016b54e152b9471
PhilW <phil@pwood.net>
2019-01-16 11:56:19 +0000

    First commit

---
+++ Blob/Blob.vb
@@ -0,0 +1,40 @@
+﻿Public Class Blob
+
+    Private positionX As Integer
+    Private positionY As Integer
+    Private size As Integer
+
+    Public Sub New()
+
+        size = 15
+
+        ' put the player in the middle
+        positionX = MainForm.PictureBox1.Width \ 2
+        positionY = MainForm.PictureBox1.Height \ 2
+
+    End Sub
+
+    Public Sub Draw(g As Graphics)
+
+        Dim brush As New SolidBrush(Color.MidnightBlue)
+
+        g.FillEllipse(brush, positionX, positionY, size, size)
+
+    End Sub
+
+    Public Sub Move(direction As String)
+
+        Select Case direction
+            Case "N"
+                positionY -= 5
+            Case "W"
+                positionX -= 5
+            Case "S"
+                positionY += 5
+            Case "E"
+                positionX += 5
+        End Select
+
+    End Sub
+
+End Class
---
+++ Blob/MainForm.vb
@@ -0,0 +1,37 @@
+﻿Public Class MainForm
+
+    Private player As Blob
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        player = New Blob
+
+    End Sub
+
+    Private Sub PictureBox1_Paint(sender As Object, e As PaintEventArgs) Handles PictureBox1.Paint
+
+        player.Draw(e.Graphics)
+
+    End Sub
+
+    Private Sub MainForm_KeyPress(sender As Object, e As KeyPressEventArgs) Handles MyBase.KeyPress
+
+        Dim Key As String
+
+        Key = e.KeyChar.ToString.ToUpper
+
+        Select Case Key
+            Case "W"
+                player.Move("N")
+            Case "A"
+                player.Move("W")
+            Case "S"
+                player.Move("S")
+            Case "D"
+                player.Move("E")
+        End Select
+
+        PictureBox1.Refresh()
+
+    End Sub
+End Class
